38004e520f302a923b73a71dc9bb444fc70e9061
Marianna Shuniaieva <shuniaevamarianna@gmail.com>
2024-11-01 13:45:27 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
-import { useState } from "react";
+import { Children, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+// import Ten from "../src/components/Tenth";
 //import FirstEx from "./components/FirstExampel";
 //import SecondExampel from "./components/SecondExampel";
 // import Six from "./components/Sixth";
 // import Seven from "./components/Seven";
 // import Eight from "./components/Eight";
-import Nine from "./components/Nine";
+// import Nine from "./components/Nine";
+// import Eleven from "./components/Eleven";
+// import ThirdTeen from "./components/Thirteen";
+// import Password from "./components/password";
+// import Fourteen from "./components/Fourteen";
+import Fiveteen from "./components/Fiveteen";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Nine />
+      {/* <Password /> */}
+      {/* <Fourteen /> */}
+      <Fiveteen />
     </>
   );
 }
--- src/components/Eight.jsx
+++ src/components/Eight.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { useState } from "react";
+import "../index.css";
 const LanguagesSelector = () => {
   const [languages, setLanguages] = useState("en");
   //   if (LanguagesSelector  === true){
